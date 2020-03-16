da9360147c31dbedd5364eec87477b8daae5aabb
Mitch D. Lincoln <catalin491@gmail.com>
2020-03-16 17:56:36 +0100

    :clown_face: Aggiunto la directory publi con file html e css. Creato una Static Route per la directory "public"

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 const express = require('express');
+const path = require('path');
 
 const app = express();
 
-app.get('/', (req, res) => {
-    res.send('Hello World, Forse');
-})
+// app.get('/', (req, res) => {
+//     res.sendFile(path.join(__dirname, 'public', 'index.html'));
+// })
+
+// SET A STATIC FOLDER
+app.use(express.static(path.join(__dirname, 'public')));
 
 const PORT = process.env.PORT || 5000;
 
