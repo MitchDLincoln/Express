362a54f96aad1627414ba5ac319f171e1bcdd9bf
Mitch D. Lincoln <catalin491@gmail.com>
2020-03-16 18:25:07 +0100

    const members = [

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
 const express = require('express');
 const path = require('path');
+const members = require('./members');
 
 const app = express();
 
-// app.get('/', (req, res) => {
-//     res.sendFile(path.join(__dirname, 'public', 'index.html'));
-// })
+// Get all Members
+app.get('/api/members', (req, res) => res.json(members));
 
 // SET A STATIC FOLDER
 app.use(express.static(path.join(__dirname, 'public')));
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => console.log(`Server started on port ${PORT}`)); 
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));  
---
+++ members.js
@@ -0,0 +1,16 @@
+const members = [
+    {
+        id : 1,
+        name : 'John Doe',
+        email : 'johndoe@gamil.com',
+        status  : 'active'
+    },
+    {
+        id : 2,
+        name : 'John Cena',
+        email : 'johncena@gamil.com',
+        status  : 'inactive'
+    }
+];
+
+module.exports = members;
