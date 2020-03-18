ae79fcf91a5fbc9526b888a6800053f783ad1fe3
Mitch D. Lincoln <catalin491@gmail.com>
2020-03-19 00:06:50 +0100

    :arrows_counterclockwise: Aggiornato, tramite Axios, le richieste GET al server!

--- schema.js
+++ schema.js
@@ -1,3 +1,4 @@
+const axios = require("axios");
 const {
   GraphQLObjectType,
   GraphQLString,
@@ -6,18 +7,6 @@ const {
   GraphQLList,
   GraphQLNonNull
 } = require("graphql");
-
-// Hardcoded Data
-const customers = [
-  { id: "1", name: "John Doe", email: "johndoe@gmail.com", age: 24 },
-  {
-    id: "1",
-    name: "Antohine Grizman",
-    email: "antohinegrizu@gmail.com",
-    age: 27
-  },
-  { id: "1", name: "Mitch D. Lincoln", email: "catalin491@gmail.com", age: 24 }
-];
 
 // Customer Type
 const CustomerType = new GraphQLObjectType({
@@ -40,17 +29,17 @@ const RootQuery = new GraphQLObjectType({
         id: { type: GraphQLString }
       },
       resolve(parentValue, args) {
-        for (let i = 0; i < customers.length; i++) {
-          if (customers[i].id === args.id) {
-            return customers[i];
-          }
-        }
+        return axios
+          .get("http://localhost:3000/customers/" + args.id)
+          .then(res => res.data);
       }
     },
     customers: {
       type: new GraphQLList(CustomerType),
       resolve(parentValue, args) {
-        return customers;
+        return axios
+          .get("http://localhost:3000/customers/")
+          .then(res => res.data );
       }
     }
   }
