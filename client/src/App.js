79dca48bed5021d647fdcda84d8daa63be1ea21e
Mitch D. Lincoln <catalin491@gmail.com>
2020-03-19 22:49:45 +0100

    :art: Importato i lanci e aggironato l'interfaccia!

--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,21 @@
 import React, { Component } from "react";
 import "./App.css";
 import logo from "./spacex_logo_white.png";
-import ApolloClient, { from } from "apollo-boost";
+import ApolloClient from "apollo-boost";
 import { ApolloProvider } from "@apollo/react-hooks";
+import Launches from "./components/Launches.js";
 
 const client = new ApolloClient({
-  uri: "http://locahost:5000/graphql"
+  uri: "http://localhost:5000/graphql"
 });
 
 class App extends Component {
   render() {
     return (
       <ApolloProvider client={client}>
-        <div className="container">
-          <img src={logo} alt="SpaceX" />
+        <div className="container d-flex flex-column justify-content-center">
+          <img src={logo} alt="SpaceX" className="rounded mx-auto d-block" />
+          <Launches />
         </div>
       </ApolloProvider>
     );
