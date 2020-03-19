d9995a640de819c5b7a7405bf51497e837e3b209
Mitch D. Lincoln <catalin491@gmail.com>
2020-03-19 18:09:58 +0100

    :tada: Inizializzato ad Apollo!

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,22 @@
 import React, { Component } from "react";
 import "./App.css";
-import logo from './spacex_logo_white.png'
+import logo from "./spacex_logo_white.png";
+import ApolloClient, { from } from "apollo-boost";
+import { ApolloProvider } from "@apollo/react-hooks";
+
+const client = new ApolloClient({
+  uri: "http://locahost:5000/graphql"
+});
 
 class App extends Component {
   render() {
-    return <div className="container">
-      <img src={logo} alt="SpaceX"/>
-    </div>;
+    return (
+      <ApolloProvider client={client}>
+        <div className="container">
+          <img src={logo} alt="SpaceX" />
+        </div>
+      </ApolloProvider>
+    );
   }
 }
 
