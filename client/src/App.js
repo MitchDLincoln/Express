b299fda7cf2cf9e1761ed76de9577d9601eea4dd
Mitch D. Lincoln <catalin491@gmail.com>
2020-03-24 00:31:41 +0100

    :pencil: Sistemento il codice e aggiunto i percorsi!

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,14 @@
 import React, { Component } from "react";
-import "./App.css";
-import logo from "./spacex_logo_white.png";
 import ApolloClient from "apollo-boost";
 import { ApolloProvider } from "@apollo/react-hooks";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+
+//Componenti
 import Launches from "./components/Launches.js";
+import Launch from "./components/Launch.js";
+//Stile ed Immagine
+import logo from "./spacex_logo_white.png";
+import "./App.css";
 
 const client = new ApolloClient({
   uri: "http://localhost:5000/graphql"
@@ -13,10 +18,13 @@ class App extends Component {
   render() {
     return (
       <ApolloProvider client={client}>
-        <div className="container d-flex flex-column justify-content-center text-center">
-          <img src={logo} alt="SpaceX" className="rounded mx-auto d-block" />
-          <Launches />
-        </div>
+        <Router>
+          <div className="container d-flex flex-column justify-content-center text-center">
+            <img src={logo} alt="SpaceX" className="rounded mx-auto d-block" />
+            <Route exact path="/" component={Launches} />
+            <Route exact path="/launch/:flight_number" component={Launch} />
+          </div>
+        </Router>
       </ApolloProvider>
     );
   }
